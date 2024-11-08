b6e4424dac6ae7fe1c3356c3a06913f076530115
kris <krismwasgit@gmail.com>
2024-11-08 12:23:11 +0300

    container style

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,3 +1,14 @@
+const containerStyle = { display: "flex", alignItems: "center", gap: "16px" };
+
 export default function StarRating() {
-  return <div>Hello</div>;
+  return (
+    <div style={containerStyle}>
+      <div>
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+      </div>
+      <p>5</p>
+    </div>
+  );
 }
